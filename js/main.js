99d2b8fa0188760874f1120fd67e892dbc7fde94
Igor Freitas <igoreted@gmail.com>
2021-03-16 11:47:32 -0300

    adicionado o bar-progress do bootstrap para mostrar que a página está sendo executada normalmente

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //alert("Foi po!!")
 function consultaCep(){
 
+    $(".barra-progresso").hide();
     var cep = document.getElementById("meu_cep").value ;
-    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    var url = "https://viacep.com.br/ws/" + cep + "/json/"; $("#powres")
     
     console.log(url);
-
 
     $.ajax({
         url: url,
@@ -18,6 +18,14 @@ function consultaCep(){
             $("#localidade").html(response.localidade)
             $("#uf").html(response.uf)
             $("#titulo_cep").html("CEP: " + response.cep)
+            $(".cep").show();
+            $(".barra-progresso").fadeIn('slow');
+            $("porwrer");document.getElementById('Powiro')
+
         }
     })
 }
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
